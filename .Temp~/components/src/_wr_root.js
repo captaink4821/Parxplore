1b036270f510c5e73b68474367f91f3603828be3
Billy Kirkpatrick <captaink4821@gmail.com>
2016-03-22 22:35:32 -0400

    transferred files from classProject1

---
+++ .Temp~/components/src/_wr_root.js
@@ -0,0 +1,45 @@
+
+
+
+	import * as mod0 from "wrm\/comp\/FormService";
+	wrm.defineModule("wrm\/comp\/FormService", mod0)
+
+	import * as mod1 from "wrm\/comp\/SelectorService";
+	wrm.defineModule("wrm\/comp\/SelectorService", mod1)
+
+	import * as mod2 from "wrm\/comp\/val\/EMailValidationRuleService";
+	wrm.defineModule("wrm\/comp\/val\/EMailValidationRuleService", mod2)
+
+	import * as mod3 from "wrm\/comp\/val\/MandatoryValidationRuleService";
+	wrm.defineModule("wrm\/comp\/val\/MandatoryValidationRuleService", mod3)
+
+	import * as mod4 from "wrm\/comp\/DetailsService";
+	wrm.defineModule("wrm\/comp\/DetailsService", mod4)
+
+	import * as mod5 from "wrm\/comp\/MessageService";
+	wrm.defineModule("wrm\/comp\/MessageService", mod5)
+
+	import * as mod6 from "wrm\/comp\/ListService";
+	wrm.defineModule("wrm\/comp\/ListService", mod6)
+
+	import * as mod7 from "wrm\/comp\/CreateService";
+	wrm.defineModule("wrm\/comp\/CreateService", mod7)
+
+	import * as mod8 from "wrm\/comp\/UpdateService";
+	wrm.defineModule("wrm\/comp\/UpdateService", mod8)
+
+	import * as mod9 from "wrm\/comp\/SwitchService";
+	wrm.defineModule("wrm\/comp\/SwitchService", mod9)
+
+	import * as mod10 from "wrm\/comp\/DeleteService";
+	wrm.defineModule("wrm\/comp\/DeleteService", mod10)
+
+	import * as mod11 from "wrm\/comp\/LoginService";
+	wrm.defineModule("wrm\/comp\/LoginService", mod11)
+
+	import * as mod12 from "wrm\/comp\/RegisterService";
+	wrm.defineModule("wrm\/comp\/RegisterService", mod12)
+
+	import * as mod13 from "wrm\/comp\/LogoutService";
+	wrm.defineModule("wrm\/comp\/LogoutService", mod13)
+
